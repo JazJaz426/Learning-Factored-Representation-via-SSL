2299bef2570b26878bafcaf674196ae6a2af9d70
Shreyas Raman <shreyas_sundara_raman@brown.edu>
2024-11-14 14:26:55 -0500

    adding obs pairs generation to  dataset.py

--- data/dataset.py
+++ data/dataset.py
@@ -23,7 +23,7 @@ class CustomDataset(Dataset):
         self.data_env = DataGenerator(data_env_config)
         self.count = 0
         self.limit = limit
-        self.mode = mode #options: seq [sequential], cont [controlled factors], triplet [triplet pair with different actions], rand [random reset, inbuilt]
+        self.mode = mode #options: seq [sequential], cont [controlled factors], triplet [triplet pair with different actions], rand [random reset, inbuilt], step [returns s1, s2, a as state pair]
         self.classes = list(range(1000))
 
         #policy model used for data generation: load from checkpoint path if needed
@@ -67,12 +67,21 @@ class CustomDataset(Dataset):
         if self.mode == 'seq':
 
             #get the current visual observation and underlying state
-            obs = self.data_env.get_curr_obs()
-            state, norm_state = self._construct_state()
+            obs_pre = self.data_env.get_curr_obs()
+            state_pre, norm_state_pre = self._construct_state()
             
             #predict action and take a step in the environment
-            actions, __ = self.model.predict(obs, deterministic=True)
-            self.data_env.step(actions)
+            action, __ = self.model.predict(obs, deterministic=True)
+            self.data_env.step(action)
+
+            #get the future visual observation and underlying state
+            obs_post = self.data_env.get_curr_obs()
+            state_post, norm_state_post = self._construct_state()
+            
+            #NOTE: only for sequential data generation, use the policy to output a
+            return (obs_pre, obs_post), (state_pre, state_post), (norm_state_pre, norm_state_post), action
+
+
 
         elif self.mode == 'cont':
             #NOTE: input controlled_factors as empty dictionary so that all factors are randomized following env rules
@@ -86,6 +95,8 @@ class CustomDataset(Dataset):
             obs, info = self.data_env.reset()
             state, norm_state = self.data_env._construct_state()
 
+            
+
         elif self.mode == 'triplet':
             raise NotImplementedError(f'ERROR: data generation mode cannot be {self.mode}')
         else:
@@ -93,7 +104,7 @@ class CustomDataset(Dataset):
 
         self.count += 1
 
-        return obs, norm_state, state 
+        return obs, norm_state, state, None
 
 
 class RandomPolicy:
@@ -117,9 +128,9 @@ if __name__ == "__main__":
 
     os.makedirs('./temp', exist_ok = True)
 
-    for i,d in enumerate(dataset):
+    for i, (obs, norm_state, state, action) in enumerate(dataset):
 
-        img = Image.fromarray(d[0])
+        img = Image.fromarray(obs)
         img.save(f'./temp/{i+1}.png')
         print(i+1)
         print(dataset.count)
